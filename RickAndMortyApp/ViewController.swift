880f007081f796de923d2e3fb22f8e9b8251fdb0
Hamza Abdulilah <hamza.abdulilah@smartrefill.se>
2019-09-07 14:08:35 +0200

    project created

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Hamza Abdulilah on 2019-09-07.
+//  Copyright © 2019 Hamza Abdulilah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
